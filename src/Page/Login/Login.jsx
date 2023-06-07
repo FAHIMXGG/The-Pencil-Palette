941900731db335f6b5d34d8c5442f3974ca8ead6
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-06-07 09:32:39 +0600

    login

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                Swal.fire({
+                    title: 'User Login Successful.',
+                    showClass: {
+                        popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__fadeOutUp'
+                    }
+                });
+                
+            })
+    }
     return (
-        <div>
-            <h1>login</h1>
-        </div>
+        <>
+            
+            <div className="hero min-h-screen bg-base-200 rounded-xl">
+                <div className="hero-content flex-row ">
+                        <h1>SingUp</h1>
+                        <img className="w-40" src="https://adrack.com/wp-content/uploads/2021/12/signup.png" alt="" />
+                    
+                    <div className="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                                {errors.email && <span className="text-red-600">Email is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password"  {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 15,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                })} placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                        </form>
+                        <p className="text-center mb-5"><small>Already have an account <Link to="/login">Login</Link></small></p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
