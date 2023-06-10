d4a231b988c93b12d4567862e72c70463286e199
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-06-10 13:23:22 +0600

    add user

--- src/Page/DashBoard/AllUsers/AllUsers.jsx
+++ src/Page/DashBoard/AllUsers/AllUsers.jsx
@@ -1,12 +1,33 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
+import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
+
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
 
     const handleDelete = user =>{
 
@@ -34,7 +55,9 @@ const AllUsers = () => {
                                 <th>{index + 1}</th>
                                 <td>{user.email}</td>
                                 <td>{user.name}</td>
-                                <td>{user.role}</td>
+                                <td>{ user.role === 'admin' ? 'admin' :
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-xs btn-ghost bg-orange-600  text-white"><FaUserShield></FaUserShield></button> 
+                                    }</td>
                                 <td><button onClick={() =>handleDelete(user)} className="btn btn-ghost btn-xs bg-red-800"><FaTrashAlt></FaTrashAlt></button></td>
                             </tr>)
                         }
